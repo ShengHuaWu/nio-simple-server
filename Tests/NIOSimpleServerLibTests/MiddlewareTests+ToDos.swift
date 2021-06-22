ccd5ab0d8ddf3d4e92f1153590c290562d956847
ShengHua Wu <swu1@wayfair.com>
2021-06-22 09:31:32 +0200

    Append test cases for updating and deletion.

--- Tests/NIOSimpleServerLibTests/MiddlewareTests+ToDos.swift
+++ Tests/NIOSimpleServerLibTests/MiddlewareTests+ToDos.swift
@@ -70,9 +70,100 @@ final class MiddlewareToDosTests: XCTestCase {
         XCTAssertEqual(response.body, try? environment.jsonEncoder().encode(item))
     }
     
-    
     func testGetOneByIdFailsWhenItemNotFound() throws {
         let response = Middleware.todos.run(&state, .get(id: "Blob"), environment)
+        
+        XCTAssertEqual(response.statusCode, 404)
+        XCTAssertTrue(response.headers.isEmpty)
+        
+        let apiError = try JSONDecoder().decode(APIError.self, from: response.body)
+        // TODO: Assert `response.body` with the private error
+        XCTAssertFalse(apiError.message.isEmpty)
+        XCTAssertFalse(apiError.errorDump.isEmpty)
+    }
+    
+    func testUpdateOneByIdSucceeds() {
+        environment.jsonEncoder = {
+            let encoder = JSONEncoder()
+            encoder.dateEncodingStrategy = .millisecondsSince1970
+            
+            return encoder
+        }
+        
+        environment.now = {
+            Date(timeIntervalSince1970: 645606000)
+        }
+        
+        let id = "XYZ"
+        let item = ToDoItem(
+            id: id,
+            description: "This is a placeholder",
+            dueTo: Date(),
+            createdAt: Date(),
+            updatedAt: Date()
+        )
+        state.todos = [item]
+        
+        let body = UpdateTodoItemBody(description: "New description", dueTo: Date())
+        let response = Middleware.todos.run(&state, .update(id: id, body: body), environment)
+        
+        XCTAssertEqual(response.statusCode, 200)
+        XCTAssertTrue(response.headers.isEmpty)
+        
+        let expectedItem = ToDoItem(
+            id: id,
+            description: body.description!,
+            dueTo: body.dueTo!,
+            createdAt: item.createdAt,
+            updatedAt: environment.now()
+        )
+        XCTAssertEqual(response.body, try? environment.jsonEncoder().encode(expectedItem))
+        
+        XCTAssertEqual(state.todos.count, 1)
+        XCTAssertEqual(state.todos.first, expectedItem)
+    }
+    
+    func testUpdateOneFailsWhenItemNotFound() throws {
+        let body = UpdateTodoItemBody(description: "New description", dueTo: Date())
+        let response = Middleware.todos.run(&state, .update(id: "Blob", body: body), environment)
+        
+        XCTAssertEqual(response.statusCode, 404)
+        XCTAssertTrue(response.headers.isEmpty)
+        
+        let apiError = try JSONDecoder().decode(APIError.self, from: response.body)
+        // TODO: Assert `response.body` with the private error
+        XCTAssertFalse(apiError.message.isEmpty)
+        XCTAssertFalse(apiError.errorDump.isEmpty)
+    }
+    
+    func testDeleteOneByIdSucceeds() {
+        environment.jsonEncoder = {
+            let encoder = JSONEncoder()
+            encoder.dateEncodingStrategy = .millisecondsSince1970
+            
+            return encoder
+        }
+        
+        let id = "XYZ"
+        let item = ToDoItem(
+            id: id,
+            description: "This is a placeholder",
+            dueTo: Date(),
+            createdAt: Date(),
+            updatedAt: Date()
+        )
+        state.todos = [item]
+        
+        let response = Middleware.todos.run(&state, .delete(id: id), environment)
+        
+        XCTAssertEqual(response.statusCode, 204)
+        XCTAssertTrue(response.headers.isEmpty)
+        XCTAssertEqual(response.body, try? environment.jsonEncoder().encode(item))
+        XCTAssertTrue(state.todos.isEmpty)
+    }
+    
+    func testDeleteOneByIdFailsWhenItemNotFound() throws {
+        let response = Middleware.todos.run(&state, .delete(id: "Blob"), environment)
         
         XCTAssertEqual(response.statusCode, 404)
         XCTAssertTrue(response.headers.isEmpty)
